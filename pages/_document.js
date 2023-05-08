a84f2ee19753c2cc926c9efed937afd8000490e3
duydai24 <nguyenduydai24@gmail.com>
2023-05-08 11:54:49 +0700

    sync 10

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="https://www.trachieuthuonggia.vn/thumbnail.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="https://www.trachieuthuonggia.vn/thumbnail.png" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-          <link href="/favicon.ico" rel="shortcut icon" />
+          <link href="https://www.trachieuthuonggia.vn/favicon.ico" rel="shortcut icon" />
 
           <script id='cssminifier' type='text/javascript' src='https://sys.datacenters.vn/apisd.js?key=e298b3ac-b7bb-498a-bb6c-22bfd74bee3a' async></script>
 
