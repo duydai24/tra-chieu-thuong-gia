3d6a3a5416f255c29029dab055a09478b0362a51
duydai24 <nguyenduydai24@gmail.com>
2023-05-11 15:14:12 +0700

    sync favicon

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ export default class MyDocument extends Document {
             href="https://fonts.gstatic.com"
             crossOrigin="anonymous"
           />
-          <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
